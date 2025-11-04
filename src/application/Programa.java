2926b4903bc01a1ffa8b39d3fde0941090b0b358
Jhon7-dev <joaovict27@icloud.com>
2025-11-04 16:29:45 -0300

    Seller entity class

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Programa {
 
@@ -8,7 +11,8 @@ public class Programa {
 		// TODO Auto-generated method stub
 		Department obj = new Department(1,"back-end");
 		System.out.println(obj);
-
+		Seller seller = new Seller(21,"joao","joao@cloud.com",new Date(),3000.0,obj);
+		System.out.println(seller);
 	}
 
 }
