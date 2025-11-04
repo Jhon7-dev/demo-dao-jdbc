8de6e72c4ff7f54fd2e29acc6b4199242c9b4046
Jhon7-dev <joaovict27@icloud.com>
2025-11-04 15:41:19 -0300

    Department entity class

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department obj = new Department(1,"back-end");
+		System.out.println(obj);
+
+	}
+
+}
